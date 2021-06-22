f1d7d0e4fff22f1774d561da6067f9421ae8773f
Luciano <calieno@gmail.com>
2021-06-22 19:57:05 -0300

    Segundo Dia

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,12 @@
-import "reflect-metadata";
+import { router } from "./routes"
+import "reflect-metadata"
 
-import express, { request, response } from "express";
+import express, { request, response } from "express"
 
 import "./database"
 
-const app = express();
-app.listen(3000, () => console.log ("\n\n-------------------- \nServer na porta 3000"));
+const app = express()
+app.use(express.json())
+app.use(router)
+
+app.listen(3000, () => console.log ("\n\n-------------------- \nServer na porta 3000"))
