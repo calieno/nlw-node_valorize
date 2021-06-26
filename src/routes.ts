fed28729798fc1c6c6bfd6b7a472e3bffa47ac09
Luciano Ribeiro <calieno@MacBook-Air-de-Luciano.local>
2021-06-26 07:00:28 -0300

    Terceiro dia e mais um pouco

--- src/routes.ts
+++ src/routes.ts
@@ -2,13 +2,16 @@ import { Router } from "express"
 import { CreateUserController } from "./controllers/CreateUserController"
 import { CreateTagController } from "./controllers/CreateTagController"
 import { ensurAdmin } from "./middlewares/ensureAdmin"
+import { AuthenticateUserControler } from "./controllers/AutenticateUserController"
 
 const router = Router()
 
 const createUserController = new CreateUserController
 const createTagController = new CreateTagController
+const authenticateUserControler = new AuthenticateUserControler
 
 router.post("/users", createUserController.handle)
 router.post("/tags", ensurAdmin, createTagController.handle)
+router.post("/login", authenticateUserControler.handle)
 
 export { router }
--- src/services/CreateUserService.ts
+++ src/services/CreateUserService.ts
@@ -1,14 +1,16 @@
 import { getCustomRepository } from "typeorm"
 import { UserRepositories } from "../repositories/UserRepositories"
+import { hash } from "bcryptjs"
 
 interface IUserRequest {
-    name: string;
-    email: string;
+    name: string
+    email: string
+    password: string
     admin?: boolean
 }
 
 class CreateUserService {
-    async execute({ name, email, admin }: IUserRequest ){
+    async execute({ name, email, admin, password }: IUserRequest ){
         const userRepository = getCustomRepository(UserRepositories)
 
         if (!email) {
@@ -22,10 +24,12 @@ class CreateUserService {
             throw new Error("User already exists")
         }
 
+        const passwordHash = await hash(password, 8)
         const user = userRepository.create({
             name,
             email,
-            admin
+            admin,
+            password: passwordHash
         })
 
         await userRepository.save(user)
