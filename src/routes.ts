8b9e51fbcd9625333ce43554fe0c5ada22531ef7
Luciano Ribeiro <calieno@MacBook-Air-de-Luciano.local>
2021-06-26 15:24:16 -0300

    Terceiro dia e mais um pouco a mais

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,10 @@
-import { Router } from "express"
+import { response, Router } from "express"
 import { CreateUserController } from "./controllers/CreateUserController"
 import { CreateTagController } from "./controllers/CreateTagController"
 import { ensurAdmin } from "./middlewares/ensureAdmin"
 import { AuthenticateUserControler } from "./controllers/AutenticateUserController"
 import { CreateComplimentsController } from "./controllers/CreateComplimentsController"
+import { request } from "http"
 
 const router = Router()
 
@@ -16,5 +17,8 @@ router.post("/users", createUserController.handle)
 router.post("/tags", ensurAdmin, createTagController.handle)
 router.post("/login", authenticateUserControler.handle)
 router.post("/compliments", createComplimentsController.handle )
+router.get("/", (request, response) => {
+    response.send("- NLW -")
+})
 
 export { router }
