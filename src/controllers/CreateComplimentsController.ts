120c83ea06da80834153fdfaa27695d78cfce0eb
Luciano <calieno@gmail.com>
2021-06-27 08:02:00 -0300

    NLW Concluido

--- src/controllers/CreateComplimentsController.ts
+++ src/controllers/CreateComplimentsController.ts
@@ -3,13 +3,14 @@ import { CreateComplimentService } from "../services/CreateComplimentsService"
 
 class CreateComplimentsController{
     async handle(request: Request, response: Response){
-        const { tag_id, user_receiver, user_sender, message } = request.body
+        const { tag_id, user_receiver, message } = request.body
+        const { user_id } = request
         const createComplimentsService = new CreateComplimentService()
 
         const compliment = await createComplimentsService.execute({
             tag_id,
             user_receiver,
-            user_sender,
+            user_sender: user_id,
             message
         })
 
--- src/middlewares/ensureAdmin.ts
+++ src/middlewares/ensureAdmin.ts
@@ -1,7 +1,14 @@
 import { Request, Response, NextFunction } from "express"
+import { getCustomRepository } from "typeorm"
+import { UserRepositories } from "../repositories/UserRepositories"
 
-export function ensurAdmin(request: Request, response: Response, next: NextFunction){
-    const admin = true
+export async function ensurAdmin(request: Request, response: Response, next: NextFunction){
+
+    const { user_id } = request
+    
+    const userRepositories = getCustomRepository(UserRepositories)
+    
+    const { admin } = await userRepositories.findOne(user_id)
 
     if(admin){
         return next()
